1a618bb8a7a7301d69b6e0ed17048669e0f3e712
Leonides <xxluckyleoxx1@gmail.com>
2021-04-05 11:23:14 -0400

    commit tree f4e26dfa695960480f609935289b389ddf884917

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+if(someValue <= 400){
+  return 'This one is on me!';
+}
+if(someValue > 2000){
+  if(someValue > 2500){
+    return 'No can do.';
+  }
+  return 'I will gladly take your thirty bucks.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(true){
+    case tip === 'generous':
+      return 'Thank you so much.';
+    case tip === 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
 }
